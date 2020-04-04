25316570bedb3aa2ce65fafab6b63d8d18863a9d
tomebore <akylaimanalieva@gmail.com>
2020-04-04 18:07:53 +0600

    first commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    notes_file = open("notes.txt", "r", encoding="utf-8")
+    notes_list = [row for row in notes_file]
+    notes_file.close()
+    return render_template('index.html', notes_list=notes_list)
+
+@app.route("/hello/<string:user_name>")
+def hello(user_name):
+    user_name = user_name.capitalize()
+    return render_template("hello.html", user_name=user_name)
+
+
+@app.route("/add")
+def add():
+    return render_template("add.html")
+
+
+@app.route("/add-note", methods=["POST"])
+def add_note():
+    note = request.form.get("note")
+    notes_file = open('notes.txt', 'a+', encoding="utf-8")
+    notes_file.write(str(note) + "\n")
+    notes_file.close()
+    return render_template("success.html")
+
+
+
+
+
+
+    
+
+# https://github.com/Amankaium/notes/blob/master/templates/hello.html
